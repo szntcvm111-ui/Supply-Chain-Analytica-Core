cf0631a02fffe1b126bb86fadb25064d0a9f04e6
szntcvm111-ui <szntcvm111@gmail.com>
2026-02-25 21:01:13 +0700

    cleanup internal assets

--- Bounties_Submissions/Metamask_Balance_Fix.js
+++
@@ -1,7 +0,0 @@
-// Metamask Mobile UI Balance Refresh Fix
-function forceRefreshBalance() {
-  const current_state = getWalletState();
-  if (current_state.needsUpdate) {
-    provider.refresh(); // Fixed: ensuring immediate state sync
-  }
-}
