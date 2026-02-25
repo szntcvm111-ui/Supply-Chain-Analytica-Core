425369698a02d5b9f49efe84244b2dc96f676fd7
szntcvm111-ui <szntcvm111@gmail.com>
2026-02-25 21:01:13 +0700

    cleanup internal assets

--- Bounties_Submissions/Eth_Scanner_Filter_Fix.py
+++
@@ -1,6 +0,0 @@
-// Ethereum Block Scanner Filter Logic Correction
-def parse_block_filter(block_hash):
-    # Fixed: added support for hex string prefix validation
-    if not block_hash.startswith("0x"):
-        block_hash = "0x" + block_hash
-    return fetch_data(block_hash)
