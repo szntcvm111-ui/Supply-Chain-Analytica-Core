7f86e4bf504704d2b99e927a1a9fd2cf6729b75f
szntcvm111-ui <szntcvm111@gmail.com>
2026-02-25 21:01:15 +0700

    cleanup internal assets

--- Bounties_Submissions/Uniswap_UI_Fix.ts
+++
@@ -1,6 +0,0 @@
-// Translated Interface Correction for Uniswap / RU
-const translations = {
-  "Slippage": "Проскальзывание (Slippage)",
-  "Liquidity Pool": "Пул ликвидности",
-  "Transaction Settings": "Настройки транзакции"
-}
